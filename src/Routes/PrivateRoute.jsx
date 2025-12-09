b67a30318c840a40be739450600ca2fec51e06b2
417MahirRahman <35mahirrahman@gmail.com>
2025-12-09 22:36:13 +0600

    added Private Route and Loader

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate, useLocation } from "react-router";
+import Loader from "../utilities/Loader";
+import { useContext } from "react";
+import AuthContext from "../providers/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) return <Loader></Loader>;
+  if (user) return children;
+  return <Navigate to="/login" state={location.pathname} replace="true" />;
+};
+
+export default PrivateRoute;
